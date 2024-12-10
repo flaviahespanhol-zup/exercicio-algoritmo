6a6dc341a1fbb46d8365c7bcafea1f0c3ce7a0eb
Flavia Hespanhol <flavia.hespanhol@zup.com.br>
2024-12-10 12:05:57 -0300

    feat: Add class BubbleSort and method bubbleSort

---
+++ src/BubbleSort.java
@@ -0,0 +1,18 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] array) {
+
+        for (int index = 0; index < (array.length - 1); index++) {
+            for (int j = 0; j < array.length - index - 1; j++) {
+                if (array[j] > array[j + 1]) {
+
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+}
