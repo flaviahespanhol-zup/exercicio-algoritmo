018cb34adf50afdb414b7148d5aedbdd862e5432
Flavia Hespanhol <flavia.hespanhol@zup.com.br>
2024-12-10 12:12:50 -0300

    feat: Apply method bubbleSort in class Main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        //Definimos um array de inteiros (numero a serem ordenados)
+        int[] arr = {24, 68, 70, 10, 5, 20};
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Exibindo o array antes da ordenação.
+        System.out.println("Array antes da ordenação: " + Arrays.toString(arr));
+
+        //Chamamos o metodo bubbleSort da classe BubbleSort.
+        BubbleSort.bubbleSort(arr);
+
+        System.out.println("Arrray depois da ordenação: " + Arrays.toString(arr));
     }
 }
